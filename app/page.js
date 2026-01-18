b4e9607b81594a98123de579f746317df3be2b63
Rithvik <vikturi99@gmail.com>
2026-01-18 11:36:05 -0600

    parentheses around style in the in article ad

--- app/page.js
+++ app/page.js
@@ -39,10 +39,10 @@ export default function Home() {
             </p>
           </article>
 
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4344533853971687"
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4344533853971687"
               crossorigin="anonymous"></script>
           <ins class="adsbygoogle"
-              style="display:block; text-align:center;"
+              style={{display: "block", textAlign: "center"}}
               data-ad-layout="in-article"
               data-ad-format="fluid"
               data-ad-client="ca-pub-4344533853971687"
